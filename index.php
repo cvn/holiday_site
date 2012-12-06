a9924b89fc99f230335368f04d44e14c8daacb82
mack <mackcg@gmail.com>
2012-12-05 20:10:05 -0800

    github rocks - Fixed Ticker
    New ticker playaaaa

--- index.php
+++ index.php
@@ -118,14 +118,14 @@
                                                 $("#CounterZone").flipCounter(
                                                    "startAnimation", // scroll counter from the current number to the specified number
                                                       {
-                                                              number: 0000000000001, // the number we want to scroll from
-                                                              end_number: 00000001024, // the number we want the counter to scroll to
+                                                              number: 1, // the number we want to scroll from
+                                                              end_number: 1024, // the number we want the counter to scroll to
                                                               easing: jQuery.easing.easeOutCubic, // this easing function to apply to the scroll.
-                                                              duration: 5000, // number of ms animation should take to complete
+                                                              duration: 2000, // number of ms animation should take to complete
                                                       //  number:0, // the initial number the counter should display, overrides the hidden field
                                                          numIntegralDigits:16, // number of places left of the decimal point to maintain
                                                       //  numFractionalDigits:0, // number of places right of the decimal point to maintain
-                                                        digitClass:"counter-digit", // class of the counter digits
+                                                        //digitClass:"counter-digit", // class of the counter digits
                                                         counterFieldName:"counter-value", // name of the hidden field
                                                         digitHeight:72, // the height of each digit in the flipCounter-medium.png sprite image
                                                         digitWidth:46, // the width of each digit in the flipCounter-medium.png sprite image
--- services/add-donation.php
+++ services/add-donation.php
@@ -17,24 +17,23 @@ try {
     $conn = new PDO('mysql:host=localhost;dbname=donations', $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Insert new donation
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':name', $name);
     $stmt->bindParam(':amount', $amount);
     $stmt->execute();
 
+    // Display what information has been inserted
     $insertedID = $conn->lastInsertId();
-
-    try {
-        $validateStmt = $conn->prepare($validateSql);
-        $validateStmt->bindParam(':id', $insertedID);
-        $validateStmt->execute();
-        while($row = $validateStmt->fetch()) {
-            print $row[name];
-            print $row[amount];
-        }
-    } catch(PDOException $e) {
-        echo 'ERROR: ' . $e->getMessage();
+    $validateStmt = $conn->prepare($validateSql);
+    $validateStmt->bindParam(':id', $insertedID);
+    $validateStmt->execute();
+    while($row = $validateStmt->fetch()) {
+        print $row[name];
+        print $row[amount];
     }
+
+    
 
 
 } catch(PDOException $e) {
