dfa0499419f4978056661d44f0e2937ffa22c263
Chad von Nau <chad@vonnau.com>
2012-12-06 13:32:39 -0800

    Adding services for adding and retrieving donations from the database

--- services/add-donation.php
+++ services/add-donation.php
@@ -1,45 +1,63 @@
 <?php
 
-$username = 'root';
-$password = 'holiday535!';
-
-$donationsTable = 'donatedTest';
-$totalAmountTable = 'totalAmountTest';
-$sql = 'INSERT INTO '.$donationsTable.' (name,amount) VALUES (:name,:amount)';
-$validateSql = 'SELECT * FROM `'.$donationsTable.'` WHERE `key` = :id';
-$totalAmountSql = 
-
-
-$name = $_REQUEST['name'];
-$amount = $_REQUEST['amount'];
-
-try {
-    $conn = new PDO('mysql:host=localhost;dbname=donations', $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Insert new donation
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':amount', $amount);
-    $stmt->execute();
-
-    // Display what information has been inserted
-    $insertedID = $conn->lastInsertId();
-    $validateStmt = $conn->prepare($validateSql);
-    $validateStmt->bindParam(':id', $insertedID);
-    $validateStmt->execute();
-    while($row = $validateStmt->fetch()) {
-        print $row[name];
-        print $row[amount];
+// This function returns a JSON object of the inserted DB row
+function addDonation($name,$amount){
+    if (!$amount) {
+        return "Donation not added. No donation amount provided.";
     }
 
-    
+    if (!$name) {
+        $name = "Anonymous Donor";
+    }
 
+    // SQL Stuff
+    $username = 'root';
+    $password = 'holiday535!';
+    $donationsTable = 'donatedTest';
+    $sql = 'INSERT INTO '.$donationsTable.' (name,amount,total) VALUES (:name,:amount,:total)';
+    $validateSql = 'SELECT * FROM `'.$donationsTable.'` WHERE `key` = :id';
+    $sumSql = 'SELECT SUM(amount) AS sumTotal FROM `'.$donationsTable;
 
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
+    try {
+        // Establish connection
+        $conn = new PDO('mysql:host=localhost;dbname=donations', $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Get sum of previous donations
+        $sumStmt = $conn->prepare($sumSql);
+        $sumStmt->execute();
+        while($row = $sumStmt->fetch()) {
+            $sum = $row[sumTotal];
+        }
+
+        // Add current amount to sum before inserting
+        $sum += $amount;
+
+        // Insert new donation
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':amount', $amount);
+        $stmt->bindParam(':total', $sum);
+        $stmt->execute();
+
+        // Retrieve what has been inserted
+        $insertedID = $conn->lastInsertId();
+        $validateStmt = $conn->prepare($validateSql);
+        $validateStmt->setFetchMode(PDO::FETCH_ASSOC);
+        $validateStmt->bindParam(':id', $insertedID);
+        $validateStmt->execute();
+        while($row = $validateStmt->fetch()) {
+            $result = $row;
+        }
+
+        // Return JSON
+        return json_encode($result);
+
+    } catch(PDOException $e) {
+        return 'ERROR: ' . $e->getMessage();
+    }
 }
 
+echo addDonation($_REQUEST['name'],$_REQUEST['amount']);
 
-
-?>  
+?>
